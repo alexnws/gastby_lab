d64b4df80398d7448ab0891952d898fd417b07b7
Alex <alex@MacBook-Air-de-Alex.local>
2023-01-30 10:27:30 +0100

    first commit

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Gatsby Lab ECV 2023`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
